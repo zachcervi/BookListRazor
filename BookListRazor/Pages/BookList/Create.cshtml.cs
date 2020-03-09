18657740f9af26e2ee7f50ab6b2bee514553b0b8
Zach Cervi <zcervi@qw-corp.com>
2020-03-08 19:29:59 -0600

    Finished Created

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -16,11 +16,25 @@ namespace BookListRazor
         {
             _db = db;
         }
-
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
